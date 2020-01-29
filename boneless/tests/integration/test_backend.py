3a3b91fa6f336412492d259d4dee13a92c12ef01
unknown <ltskinner@crimson.ua.edu>
2020-01-28 22:24:14 -0700

    added boilerplace test suite

---
+++ boneless/tests/integration/test_backend.py
@@ -0,0 +1,27 @@
+import unittest
+import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
+import boneless
+
+
+class TestIntegrationBackend(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        drinks_fixture_path = 'boneless/tests/integration/fixtures/drinks.csv'
+        self.drinks_df = pd.read_csv(drinks_fixture_path)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def test_get_drink_sizes(self):
+        query = 'limit 10'
+        drinks_df = boneless.backend.get_drink_sizes(query)
+        assert_frame_equal(drinks_df, self.drinks_df)
