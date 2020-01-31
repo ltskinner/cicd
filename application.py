b74b647b202bb974652565817b5ddf4c360ee691
unknown <ltskinner@crimson.ua.edu>
2020-01-30 19:01:26 -0700

    added build badge haha

--- application.py
+++ application.py
@@ -5,20 +5,26 @@ import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__,
+               external_stylesheets=external_stylesheets)
 
 application = app.server
 
+badge_link = "https://codebuild.us-east-2.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoiWmZFUU9ZS1NtM1JIdVFYOG0yejBucmFxR2JOYVN0YVZUcTBzSW9ZWjI1K2VCUk1mbXNWQ2NBdUZZWjBLclAxOGxnRnZBRUc4U1c4d0lJdkVreC83MXU0PSIsIml2UGFyYW1ldGVyU3BlYyI6ImR2endXajBObVptcWl3c28iLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=master"
+
 unit_0_link = 'https://cdn.vox-cdn.com/thumbor/q8z_oV3S33jSzgVdgRH-RY4KOh4=/0x0:1200x870/920x613/filters:focal(516x282:708x474):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/59535207/absolute_unit_sheep.0.jpg'
 unit_1_link = 'https://gameplay.tips/uploads/posts/2018-09/thumbs/1537385272_montagne.jpg'
+
 
 app.title = 'Boneless Media Inc.'
 app.layout = html.Div(
     children=[
         dcc.Markdown('# Absolute Units'),
-        html.Img(src=unit_0_link),
-        html.Img(src=unit_1_link)
-    ]
+        html.Img(src=badge_link, className='single_img'),
+        html.Img(src=unit_0_link, className='single_img'),
+        html.Img(src=unit_1_link, className='single_img')
+    ],
+    className='layout_div'
 )
 
 
