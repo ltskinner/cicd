60bb0ee7c8cfd594db15e24824a9312e7528623b
unknown <ltskinner@crimson.ua.edu>
2020-02-02 13:48:20 -0700

    implemented logger

--- boneless/module.py
+++ boneless/module.py
@@ -1,14 +1,17 @@
 
 from .subpackage import sub_module_function
 
+import logging
+logger = logging.getLogger()
+
 
 def module_function():
-    print('printing from module.py')
+    logger.info('printing from module.py')
     return True
 
 
 def use_sub_module_function():
-    print('accessing submodule')
+    logger.info('accessing submodule')
     sub_module_function()
     return True
 
@@ -17,6 +20,6 @@ def test_read_from_lib():
     with open('lib/banner.txt') as f:
         contents = f.read()
 
-    print('lib/banner.txt contents:')
-    print(repr(contents))
+    logger.info('lib/banner.txt contents:')
+    logger.info(repr(contents))
     return contents
