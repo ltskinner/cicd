af128e149eb1ed22cc66d5ac2d67f1bd98471aef
unknown <ltskinner@crimson.ua.edu>
2020-02-04 19:34:57 -0700

    env toggle

--- boneless/__init__.py
+++ boneless/__init__.py
@@ -1,6 +1,7 @@
 #
 from . import backend
 
+from .utilities import get_env_name
 from .app_backend import (get_unit_0, make_unit_0_callback,
                           get_unit_1, make_unit_1_callback,
                           update_unit_count_0,
